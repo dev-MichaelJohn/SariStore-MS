973c2397d1e8f802b78f70a8570d88b3be84ab2f
Michael John Larido <lakatangaming145@gmail.com>
2025-11-17 16:20:24 +0800

    added Inventory service

---
+++ server/src/service/inventory.service.ts
@@ -0,0 +1,25 @@
+import db, { ITransaction } from "../config/db.config.js";
+import { eq, and, ilike, lte, SQL } from "drizzle-orm";
+import { Inventory } from "../db/schema.db.js";
+import { Column } from "drizzle-orm";
+
+export type IInventorySelect =  typeof Inventory.$inferSelect;
+export type IInventoryInsert = typeof Inventory.$inferInsert;
+
+export default class InventoryService {
+    static async GetInventoryById(id: string): Promise<IInventorySelect | null> {
+        if(id.trim().length === 0) return null;
+        const [ inventory ] = await db.select()
+            .from(Inventory)
+            .where(eq(Inventory.id, id));
+        if(!inventory) return null;
+        return inventory;
+    };
+
+    static async GetAllInventory(): Promise<IInventorySelect[] | null> {
+        const inventories = await db.select()
+            .from(Inventory);
+        if(!inventories || inventories.length === 0) return null;
+        return inventories;
+    } 
+};
