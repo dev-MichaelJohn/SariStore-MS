88483fafbcc7df0c8ee5a227943fa5fc60b502fd
Michael John Larido <lakatangaming145@gmail.com>
2025-12-09 09:42:58 +0800

    added ProductCreatorService

--- server/src/service/productCreator.service.ts
+++ server/src/service/productCreator.service.ts
@@ -3,7 +3,21 @@ import ProductService, { IProductInsert } from "./product.service";
 import InventoryService, { IInventoryInsert } from "./inventory.service";
 import AppResponse from "../lib/response.lib";
 
+/**
+ * Wrapper service for creating a product record along with their associated inventory record
+ *
+ * @export
+ * @class ProductCreatorService 
+ */
 export default class ProductCreatorService {
+    /**
+     * Creates a new operator along with their associated person record atomically
+     *
+     * @static
+     * @param {IProductInsert} product
+     * @param {Omit<IInventorySelect, "productId">} inventory
+     * @memberof ProductCreatorService
+     */
     static async Create(product: IProductInsert, inventory: Omit<IInventoryInsert, "productId">) {
         return await db.transaction(async (tx) => {
             const productRecord = await ProductService.CreateProductViaTransaction(product, tx);
