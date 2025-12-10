0e25a1291f8922d0fe610195ad1f0803fc40694a
Michael John Larido <lakatangaming145@gmail.com>
2025-12-10 19:36:46 +0800

    added API testing for Products

--- server/src/service/product.service.ts
+++ server/src/service/product.service.ts
@@ -160,7 +160,6 @@ export default class ProductService {
         
         const product = await ProductService.GetProductById(id);
         if(!product) return null;
-
         if(product.deletedAt) throw AppResponse.BadRequest("❌ Product record doesn't exist");
 
         await tx.update(Product)
