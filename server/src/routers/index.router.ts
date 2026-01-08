5e229ce52b577407233671935217c490fa8163e0
Michael John Larido <lakatangaming145@gmail.com>
2026-01-08 19:36:14 +0800

    updated API route name

--- server/src/routers/index.router.ts
+++ server/src/routers/index.router.ts
@@ -6,6 +6,6 @@ import InventoryRouter from "./inventory.router.js";
 const v1Router = Router();
 v1Router.use("/auth", AuthRouter);
 v1Router.use("/products", ProductRouter);
-v1Router.use("/inventory", InventoryRouter);
+v1Router.use("/inventories", InventoryRouter);
 
 export default v1Router;
